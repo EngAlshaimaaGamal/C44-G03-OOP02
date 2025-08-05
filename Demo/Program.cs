b6e8beee820fd3a2f84154f1f8e2003f7a1e72f8
Microsoft <Microsoft@ALSHAIMAA>
2025-08-05 19:24:21 +0300

    Example 02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -43,6 +43,40 @@
 
             #endregion
 
+            #region Example 02
+
+            //Point p1 = new Point(10, 20);
+
+            //Console.WriteLine($"Point p1: x = {p1.x}, y = {p1.y}");
+
+            //Point P2 = new Point(30, 40); 
+            
+            //Console.WriteLine($"Point p2: x = {P2.x}, y = {P2.y}");
+
+            //P2 = p1; // copy value of p1 to P2
+
+            //Console.WriteLine("After Assign P2=P1");       
+            
+
+            //Console.WriteLine($"Point p1: x = {p1.x}, y = {p1.y}");
+
+            //Console.WriteLine($"Point p2: x = {P2.x}, y = {P2.y}"); // 10,20    
+
+
+            //p1.x = 100; // change value of P2.x 
+            //p1.y = 200; // change value of P2.y
+
+
+            //Console.WriteLine("After Assign  change P1");
+
+
+            //Console.WriteLine($"Point p1: x = {p1.x}, y = {p1.y}");
+
+            //Console.WriteLine($"Point p2: x = {P2.x}, y = {P2.y}");
+
+
+            #endregion
+
 
 
 
