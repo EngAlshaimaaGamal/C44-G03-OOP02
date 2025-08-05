20500343c10d786f8e97fc80782dccb8d8200395
Microsoft <Microsoft@ALSHAIMAA>
2025-08-05 19:07:47 +0300

    Example 01

---
+++ Demo/Point.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public struct Point
+    {
+        #region Attribute
+
+        public int x;
+        public int y;
+
+
+
+        #endregion
+
+
+        #region Constructor
+
+
+        public Point() // Default Constructore
+        {
+            x = default;
+            y = default;
+
+        }
+         public Point(int X, int Y)
+        {
+
+            this.x = X;
+            this.y = Y;
+
+
+        }
+        public Point(int Y)
+        {
+
+
+            this.y = Y;
+
+
+        }
+
+
+
+
+
+        #endregion
+
+        #region Functions
+
+        public override string ToString()
+        {
+            return $"Point X {x} Point Y {y}";
+        }
+
+
+        #endregion
+
+
+
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Struct 
+
+            #region Example 01
+            //Point p1;
+            //// declare variable (data type )
+            //// clr initialize 8 Byte in stack for p1( x ,y)
+
+            //p1 = new Point();
+
+            //// new  => with any value type  as constructor selection  not create object
+
+            //p1.x = 10;
+            //p1.y = 20;  
+            //Console.WriteLine($"Point p1: x = {p1.x}, y = {p1.y}");
+
+
+            //Console.WriteLine(p1); // return (namespace .Datatype).type Demo.Point  => object 
+            //Console.WriteLine(p1.ToString()); // Demo.Point => ToString
+
+            //Point P2 = new Point();
+
+            //P2.y = 30;
+            //Console.WriteLine($"Point p1: x = {P2.x}, y = {P2.y}");
+
+            //Console.WriteLine(" Enter the X : ");
+            //int x = int.Parse(Console.ReadLine() ?? "0");   
+            //Console.WriteLine(" Enter the Y : ");   
+            //int y = int.Parse(Console.ReadLine() ?? "0");   
+            //Point P3 = new Point(x, y);
+            //Console.WriteLine($"Point p3: x = {P3.x}, y = {P3.y}");
+
+
+
+
+
+            #endregion
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
