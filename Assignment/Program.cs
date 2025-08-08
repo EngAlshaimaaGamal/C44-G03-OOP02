610277570076ad60e613b982e2199984e2ed179a
Microsoft <Microsoft@ALSHAIMAA>
2025-08-08 09:57:40 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -16,8 +16,28 @@
 
 
             #endregion
+            #region Q2
 
 
+            //Console.WriteLine(" Enter the X : ");
+            //int x = int.Parse(Console.ReadLine() ?? "0");   
+            //Console.WriteLine(" Enter the Y : ");   
+            //int y = int.Parse(Console.ReadLine() ?? "0");
+            //Console.WriteLine(" Enter the X : ");
+            //int x2 = int.Parse(Console.ReadLine() ?? "0");
+            //Console.WriteLine(" Enter the Y : ");
+            //int y2 = int.Parse(Console.ReadLine() ?? "0");
+            //Point P3 = new Point(x, y);
+            //Point P4 = new Point(x2, y2);
+
+            //double distance = Math.Sqrt(Math.Pow(P3.X - P4.X, 2) + Math.Pow(P3.Y - P4.Y, 2));
+
+            //Console.WriteLine(distance);
+
+            ////Console.WriteLine($"Distance = {distance:F2}");
+
+
+            #endregion
 
 
         }
